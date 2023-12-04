78894f5ff180ba3abec0d621631d9ca76bcf32ee
Negar <negar.r.78.ioi@gmail.com>
2023-12-03 19:54:02 -0800

    feat: implement task03 and add new methods to Clip class

--- clip.py
+++ clip.py
@@ -1,11 +1,21 @@
+import numpy as np
 from os import path
+from enum import Enum
 from PIL import Image
 from moviepy.editor import (
+    VideoClip,
+    VideoFileClip,
     CompositeVideoClip,
     ImageClip,
+    ColorClip,
 )
 from text import Text
-from typing import Tuple
+from typing import Tuple, List
+
+
+class ClipType(Enum):
+    VIDEO_FILE_CLIP = "video_file_clip"
+    VIDEO_CLIP = "video_clip"
 
 
 def resize_image(src, width, height):
@@ -18,28 +28,48 @@ def resize_image(src, width, height):
 
 
 class Clip:
-    def __init__(self, width: int, height: int, duration: int) -> None:
-        self.width = width
-        self.height = height
-        self.duration = duration
+    def __init__(self, **kwargs) -> None:
+        self.width = kwargs.get("width")
+        self.height = kwargs.get("height")
+        self.duration = kwargs.get("duration")
         self.frame = None
+        self.video = None
+        self.background_color = None
         self.background_img = list()
         self.text = list()
         self.audio = list()
 
+    def set_video(self, clip_type: ClipType, src: str = None):
+        self.video = (
+            VideoFileClip(src)
+            if clip_type == ClipType.VIDEO_FILE_CLIP
+            else VideoClip(self.make_frame, duration=self.duration)
+        )
+
+    def make_frame(self, t):
+        return 255 * np.ones((self.height, self.width, 3), dtype=np.uint8)
+
     def set_background_img(
         self,
         src: str,
+        resize: bool,
         start_time: int = 0,
         duration: int = None,
         position: Tuple = (0, 0),
     ):
-        resized_src = resize_image(src, self.width, self.height)
+        img_src = resize_image(src, self.width, self.height) if resize else src
         self.background_img.append(
-            ImageClip(resized_src)
-            .set_duration(duration or self.duration)
+            ImageClip(img_src)
             .set_start(start_time)
+            .set_duration(duration or self.duration)
             .set_pos(position)
+        )
+
+    def set_background_color(self, bg_rgb_color_code: List[int], duration: int = None):
+        self.background_color = ColorClip(
+            size=(1080, 1920),
+            color=bg_rgb_color_code,
+            duration=duration or self.duration,
         )
 
     def set_text(self, text: Text, duration: int = None, position: Tuple = (0, 0)):
@@ -47,12 +77,14 @@ class Clip:
             text.set_duration(duration or self.duration).set_position(position)
         )
 
+    def create_composite_clip(self, *args):
+        return CompositeVideoClip([*args])
+
+    @staticmethod
     def write(
-        self,
+        clip,
         dest: str = "output_video.mp4",
         codec: str = "libx264",
         fps: int = 24,
     ):
-        CompositeVideoClip(
-            [*self.background_img, *self.text, *self.audio]
-        ).write_videofile(dest, codec=codec, fps=fps)
+        clip.write_videofile(dest, codec=codec, fps=fps)
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
-from clip import Clip
+from clip import Clip, ClipType
 from text import Text
 from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    load_dotenv()
+video_width = int(os.getenv("VIDEO_WIDTH"))
+video_height = int(os.getenv("VIDEO_HEIGHT"))
+short_duration = int(os.getenv("SHORT_DURATION"))
 
-    video_width = int(os.getenv("VIDEO_WIDTH"))
-    video_height = int(os.getenv("VIDEO_HEIGHT"))
-    short_duration = int(os.getenv("SHORT_DURATION"))
 
+def task01_run():
     text = Text(os.getenv("TASK01_FONT_SRC"), os.getenv("TASK01_MSG"))
     text = text.create_text_clip(
         color=os.getenv("TASK01_TXT_COLOR"),
@@ -18,7 +18,30 @@ if __name__ == "__main__":
         size=(int(os.getenv("TASK01_TXT_SIZE_X")), int(os.getenv("TASK01_TXT_SIZE_Y"))),
         fontsize=int(os.getenv("TASK01_TXT_FONTSIZE")),
     )
-    clip = Clip(video_width, video_height, short_duration)
-    clip.set_background_img(os.getenv("TASK01_IMG_SRC"))
+    clip = Clip(width=video_width, height=video_height, duration=short_duration)
+    clip.set_background_img(os.getenv("TASK01_IMG_SRC"), resize=True)
     clip.set_text(text, position=((50, 150)))
-    clip.write(os.getenv("TASK01_OUTPUT"))
+    Clip.write(
+        clip.create_composite_clip(*clip.background_img, *clip.text),
+        os.getenv("TASK01_OUTPUT"),
+    )
+
+
+def task03_run():
+    clip = Clip(width=video_width, height=video_height, duration=5)
+    clip.set_video(ClipType.VIDEO_CLIP)
+    clip.set_background_img(
+        "assets/task03_img.jpg", resize=False, position=("center", "center")
+    )
+    clip.set_background_color([113, 181, 184])
+    Clip.write(
+        clip.create_composite_clip(
+            clip.video, clip.background_color, *clip.background_img
+        ),
+        "outputs/task03.mp4",
+    )
+
+
+if __name__ == "__main__":
+    task01_run()
+    task03_run()
